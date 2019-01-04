5f8c2301de8655aff9d7c620b4a18ddeaef9ff36
non.fu <nonfutechaca@gmail.com>
2019-01-04 01:02:40 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//ユーザー登録
+Route::get('signup', 'Auth\RegisterController@shoRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
