cc4603616b912f4c3f0f0bf361b7b49b0280ad2c
non.fu <nonfutechaca@gmail.com>
2019-01-20 08:49:32 +0000

    create tags table

---
+++ database/migrations/2019_01_20_171945_create_microposts_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMicropostsTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('microposts_tags', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('micropost_id')->unsigned()->index();
+            $table->integer('tag_id')->unsigned()->index();
+            $table->timestamps();
+
+            //外部キー設定
+            $table->foreign('micropost_id')->references('id')->on('microposts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('microposts_tags');
+    }
+}
